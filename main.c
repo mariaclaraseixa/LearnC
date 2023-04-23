7fa5e1297b751856f6ba53ce782e1e9f757b7380
mariaclaraseixa <maria.clara.seixa@gmail.com>
2023-04-23 14:50:08 -0300

    Better comments

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#define KELVIN 273.15
 
+/**
+ * create a function that  receives the temperature in Celsius degree
+ * then convert to Kelvin degree
+ * the conversion formula is: K = C + 273.15
+ */
 float temperature (float celsius) {
-    return celsius + 273.15;
+    return celsius + KELVIN; // Conversion
 }
 
-
-int main () {
-    // create a function that  receives the temperature in Celsius degree
-    // then convert to Kelvin degree
-    // the conversion formula is: K = C + 273.15
+/**
+ * User input
+ */
+void view() {
     float kelvin, celsius;
     printf ("enter the degree in Celsius");
     scanf("%i", &celsius);
     kelvin = temperature(celsius);
     printf("the result of the conversion is: %.2f (Kelvin)\n", kelvin);
+}
+
+int main () {
+    view();
     return 0;
 }
